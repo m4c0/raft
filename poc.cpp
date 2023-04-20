124f0049df7b17a708463a16e71b7e882b0ec57e
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-20 14:39:55 +0100

    rough sketch of how layout might be done

--- poc.cpp
+++ poc.cpp
@@ -2,6 +2,64 @@ import casein;
 import quack;
 
 struct element {};
+struct state {
+  bool create_mode;
+  int items[40];
+};
+
+// lays out, raii-style
+struct vgroup {};
+struct hgroup {};
+
+// create elements
+void inputbox(const casein::event &e) {}
+void text(const casein::event &e) {}
+
+class poc {
+  state &s;
+  const casein::event &e;
+
+  void create_input() { inputbox(e); }
+  void search_input() { inputbox(e); }
+
+  void title() { text(e); }
+  void input() {
+    if (s.create_mode) {
+      create_input();
+    } else {
+      search_input();
+    }
+  }
+
+  void item(auto i) {}
+
+  void header() {
+    hgroup hg{};
+
+    title();
+    input();
+  }
+  void list() {
+    vgroup vg{};
+
+    for (auto i : s.items) {
+      item(i);
+    }
+  }
+  void footer() {}
+  void info() {}
+
+public:
+  explicit constexpr poc(state &s, const casein::event &e) : s{s}, e{e} {}
+  void root() {
+    vgroup vg{};
+
+    header();
+    list();
+    footer();
+    info();
+  }
+};
 
 template <unsigned MaxElements> class raft_layout {
   quack::instance_layout<element, MaxElements> m_il;
@@ -29,6 +87,8 @@ public:
 };
 
 extern "C" void casein_handle(const casein::event &e) {
+  static state s{};
+
   static quack::renderer r{1};
   static raft_layout<1> rl{&r};
 

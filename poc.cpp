f89d96d44fee5b1b00dce5f5a4c5f9190f1d4068
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-20 13:18:02 +0100

    setting up quack

--- poc.cpp
+++ poc.cpp
@@ -1,3 +1,10 @@
 import casein;
+import quack;
 
-extern "C" void casein_handle(const casein::event &e) {}
+extern "C" void casein_handle(const casein::event &e) {
+  static quack::renderer r{1};
+  static quack::instance_layout<bool, 1> il{&r};
+
+  r.process_event(e);
+  il.process_event(e);
+}

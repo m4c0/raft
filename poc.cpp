730ae72a9e67aeda8852a4543aadcf1e4b32370a
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-20 13:48:18 +0100

    playing around how the public class will be

--- poc.cpp
+++ poc.cpp
@@ -1,10 +1,37 @@
 import casein;
 import quack;
 
+struct element {};
+
+template <unsigned MaxElements> class raft_layout {
+  quack::instance_layout<element, MaxElements> m_il;
+
+public:
+  explicit raft_layout(quack::renderer *r) : m_il{r} {}
+
+  void update_layout() {
+    m_il.reset_grid();
+    // update layout (create elements?)
+    // fill
+    // batch->resize?
+  }
+
+  void process_event(const casein::event &e) {
+    m_il.process_event(e);
+    switch (e.type()) {
+    case casein::RESIZE_WINDOW:
+      update_layout();
+      break;
+    default:
+      break;
+    }
+  }
+};
+
 extern "C" void casein_handle(const casein::event &e) {
   static quack::renderer r{1};
-  static quack::instance_layout<bool, 1> il{&r};
+  static raft_layout<1> rl{&r};
 
   r.process_event(e);
-  il.process_event(e);
+  rl.process_event(e);
 }

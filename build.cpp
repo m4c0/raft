87c872379b3486d260cacbd8ed3a80b2880b0081
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-21 11:01:55 +0100

    build: add "native" req

--- build.cpp
+++ build.cpp
@@ -4,6 +4,7 @@ int main(int argc, char **argv) {
   using namespace ecow;
 
   auto poc = unit::create<app>("poc");
+  poc->add_requirement(native);
   poc->add_unit<>("poc");
   poc->add_wsdep("casein", casein());
   poc->add_wsdep("quack", quack());

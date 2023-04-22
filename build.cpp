d0fe614787ff5fecea3ed5879dccbe4f1c8287ee
Eduardo Costa <m4c0@users.noreply.github.com>
2023-04-22 11:26:24 +0100

    move ecow to header

--- build.cpp
+++ build.cpp
@@ -3,13 +3,11 @@
 int main(int argc, char **argv) {
   using namespace ecow;
 
-  auto m = unit::create<mod>("raft");
-
   auto poc = unit::create<app>("poc");
   poc->add_requirement(native);
   poc->add_wsdep("casein", casein());
   poc->add_wsdep("quack", quack());
-  poc->add_ref(m);
+  poc->add_ref(raft());
   poc->add_unit<>("poc");
   return run_main(poc, argc, argv);
 }
--- build.hpp
+++ build.hpp
@@ -1,3 +1,12 @@
 #include "../casein/build.hpp"
 #include "../ecow/ecow.hpp"
 #include "../quack/build.hpp"
+
+auto raft() {
+  using namespace ecow;
+
+  auto m = unit::create<mod>("raft");
+  m->add_wsdep("casein", casein());
+  m->add_wsdep("quack", quack());
+  return m;
+}
